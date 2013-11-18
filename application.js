c3f7e3e6f1785cde3c2ff9f50cc0e3989f1d8e86
Yuichi Hagio <yhagio87@gmail.com>
2013-11-18 20:22:49 +0900

    Modified CSS for arrows and visibility added with jQuery

--- application.js
+++ application.js
@@ -4,10 +4,10 @@ $(document).ready(function(){
   $('#first_a').hover(
     function(){
     $default_message.detach();
-    $('#first_a').prepend('<img class="arrow_left" src="images/arrow-left.png">');
+    $('img.arrow_left.first_box').addClass('visibility_on');
     $('div.container').append('<div class="message1"><h2><span>Basecamp</span> is the project management tool you wish you had on your last project.</h2><p>Are you still managing projects with email? Are you still using Excel for your to-do lists? It’s time to upgrade to Basecamp. Manage projects and collaborate with your team and clients the modern way.</p></div>');
   }, function(){
-    $('img.arrow_left').remove();
+    $('img.arrow_left.first_box').removeClass('visibility_on');
     $('div.message1').remove();
     $('div.container').append($default_message);
   });
@@ -15,10 +15,10 @@ $(document).ready(function(){
   $('#second_a').hover(
     function(){
     $default_message.detach();
-    $('#second_a').prepend('<img class="arrow_left" src="images/arrow-left.png">');
+    $('img.arrow_left.second_box').addClass('visibility_on');
     $('div.container').append('<div class="message2"><h2><span>Hirise</span> remembers the important things about people you’d normally forget.</h2><p>Keep a permanent record of people you do business with. Know who you talked to, when you talked to them, what was said, and when to follow up next. Over 20,000,000 contacts are tracked using Highrise.</p></div>');
   }, function(){
-    $('img.arrow_left').remove();
+    $('img.arrow_left.second_box').removeClass('visibility_on');
     $('div.message2').remove();
     $('div.container').append($default_message);
   });
@@ -26,10 +26,10 @@ $(document).ready(function(){
   $('#third_a').hover(
     function(){
     $default_message.detach();
-    $('#third_a').prepend('<img class="arrow_right" src="images/arrow-right.png">');
+    $('img.arrow_right').addClass('visibility_on');
     $('div.container').append('<div class="message3"><h2>From near or far, <span>Campfire</span> helps teams work together over the web in real-time.</h2><p>Share ideas, discussions, concepts, images, code samples, videos, mockups, and documents in a real-time private chat room. It’s game changing. We couldn’t run our own business without Campfire.</p></div>');
   }, function(){
-    $('img.arrow_right').remove();
+    $('img.arrow_right').removeClass('visibility_on');
     $('div.message3').remove();
     $('div.container').append($default_message);
   });
